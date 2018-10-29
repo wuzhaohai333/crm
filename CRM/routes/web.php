e8dce314bf9e1b8cdbb7dac89b672d6bf8c72fcc
吴兆海 <123@qq.com>
2018-10-29 11:26:28 +0800

    框架

---
+++ CRM/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/** 登录*/
+Route::get('/', function () {
+    return view('login');
+});
+/** 桌面首页*/
+Route::any('/index', function () {
+    return view('index');
+});
